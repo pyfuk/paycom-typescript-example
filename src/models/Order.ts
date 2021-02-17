d528b54a7d19c5b97cba5f5421dd7f2fa87b7827
pyfuk <shax988@gmail.com>
2021-02-17 15:58:28 +0500

    [+]Added order model

---
+++ src/models/Order.ts
@@ -0,0 +1,25 @@
+const Orders = [{id: 1, user_id: 2, products_id: [], status: 2, amount: 2}];
+
+export class Order {
+
+    id: number;
+    user_id: number;
+    status: number;
+    amount: number;
+
+
+    find(id: number){
+        const order =  Orders.find(order => order.id === id);
+
+        if(order){
+            this.id = order.id;
+            this.amount = order.amount;
+            this.status = order.status;
+            this.user_id = order.user_id;
+
+            return this;
+        }
+
+        return null;
+    }
+}
